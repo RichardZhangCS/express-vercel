3920df6450bd657f44621c1d3d8cd961873f3403
RichardZhangCS <richardzhang1303@gmail.com>
2021-12-07 17:11:18 -0500

    Added my files

--- index.js
+++ index.js
@@ -6,5 +6,11 @@ app.use(express.json({ extended: false }));
 
 app.use("/api/product", product);
 
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
